641ac9bbd942461fca9914291c74dee2daac91f4
Jeriko <jerikomarcelino@gmail.com>
2023-07-30 19:10:52 +0800

    update javascript

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
-function getRandomArray(arr){
-    const randomIndex = Math.floor(Math.random()*arr.length)
-};
+let rpsChoice = ["Rock","Paper","Scissors"]
 
-function getComputerChoice(){
+let randomRpsChoice = Math.floor(Math.random() * rpsChoice.length);
 
-};
+console.log("randomRpsChoice");
+
+// function getRandomArray(arr){
+//     const randomIndex = Math.floor(Math.random()*arr.length)
+// };
+
+// function getComputerChoice(){
+
+// };
+
+// 
